792a5a199069431b18c3ab45284b6acace50113f
Patrick <pfriis@outlook.com>
2025-09-25 15:24:39 +0200

    add welcome message to server and implement logic for calculation

--- TCPserver.py
+++ TCPserver.py
@@ -3,8 +3,37 @@ import threading
 
 # Service
 def service(connectionSocket):
-    message = connectionSocket.recv(1024).decode()
-    #TODO add service logic
+    welcomeMessage = "To start type: add, subtract or random. To disconnect type: exit."
+    connectionSocket.send(welcomeMessage.encode())
+
+    while True:
+        message = connectionSocket.recv(1024).decode().strip()
+        if not message:
+            break
+
+        if message == 'exit':
+            connectionSocket.send("You have disconnected".encode())
+            connectionSocket.close()
+            break
+
+        if message == 'add':
+            instructionsMessage = "Send two numbers as: <x> <y> Example: 2 4"
+            connectionSocket.send(instructionsMessage.encode())
+
+            calculatorParts = connectionSocket.recv(1024).decode().strip().split()
+            x = int(calculatorParts[0])
+            y = int(calculatorParts[1])
+            result = x + y
+
+            connectionSocket.send(str(result).encode())
+
+        # if message == 'subtract':
+            # TODO: add subtract logic
+            
+
+        # if message == 'random':
+            # TODO: add random logic
+            
 
 # Concurrent Server
 serverport = 21
@@ -14,4 +43,4 @@ serverSocket.listen(1)
 print('The server is ready to receive')
 while True:
     connectionSocket, addr = serverSocket.accept()
-    threading.Thread(target=service, args=(connectionSocket)).start()
+    threading.Thread(target=service, args=(connectionSocket,)).start()
