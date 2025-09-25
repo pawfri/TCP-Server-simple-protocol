60ec0becb14bed629ec531185b2080176fc51415
Patrick <pfriis@outlook.com>
2025-09-25 14:37:13 +0200

    Add Concurrent Server setup to TCPserver.py and prepared for service logic

--- TCPserver.py
+++ TCPserver.py
@@ -1,1 +1,17 @@
 from socket import *
+import threading
+
+# Service
+def service(connectionSocket):
+    message = connectionSocket.recv(1024).decode()
+    #TODO add service logic
+
+# Concurrent Server
+serverport = 21
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(('', serverport))
+serverSocket.listen(1)
+print('The server is ready to receive')
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    threading.Thread(target=service, args=(connectionSocket)).start()
